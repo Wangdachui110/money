01d6f5d3914504522ceed1762517916712165816
wdc9358 <1971571804@qq.com>
2020-10-31 16:05:31 +0800

    html和css写好了

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,28 +1,5 @@
 <template>
-    <Layout>
-      <div class="tags">
-        <ul class="current">
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ul>
-        <div class="new">
-          <button>新增标签</button>
-        </div>
-      </div>
-      <div>
-        <label class="notes">
-          <span class="name">备注</span>
-          <input type="text" placeholder="在这里输入备注">
-        </label>
-      </div>
-      <div>
-        <ul class="types">
-          <li class="selected">支出</li>
-          <li>收入</li>
-        </ul>
-      </div>
+    <Layout class-prefix="layout">
       <div class="numberPad">
         <div class="output">100</div>
         <div class="buttons">
@@ -42,6 +19,29 @@
           <button>.</button>
         </div>
       </div>
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text" placeholder="在这里输入备注">
+        </label>
+      </div>
+      <div class="tags">
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
+      </div>
     </Layout>
 </template>
 
@@ -51,6 +51,12 @@
   }
 </script>
 
+<style lang="scss">
+  .layout-content{
+    display: flex;
+    flex-direction: column-reverse;
+  }
+</style>
 <style lang="scss" scoped>
   @import "~@/assets/style/helper.scss";
   .numberPad{
@@ -145,8 +151,12 @@
   .tags {
     font-size: 14px;
     padding: 16px;
+    flex-grow: 1;
+    display: flex;
+    flex-direction: column-reverse;
     > .current {
       display: flex;
+      flex-wrap: wrap;
       > li {
         background: #d9d9d9;
         $h: 24px;
@@ -155,10 +165,13 @@
         border-radius: $h/2;
         padding: 0 16px;
         margin-right: 12px;
+        margin-top: 4px;
       }
     }
-    > .new{
+
+    > .new {
       padding-top: 16px;
+
       button {
         background: transparent;
         border: none;
