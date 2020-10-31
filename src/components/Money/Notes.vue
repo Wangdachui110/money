b877b577f6014774a8e174b8973f8bb4d42e92c3
wdc9358 <1971571804@qq.com>
2020-10-31 17:17:33 +0800

    使用js实现了一个组件

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+  <div>
     <label class="notes">
       <span class="name">备注</span>
       <input type="text" placeholder="在这里输入备注">
@@ -9,20 +9,22 @@
 
 <script lang="ts">
 export default {
-name: "Notes"
-}
+  name: 'Notes'
+};
 </script>
 
 <style lang="scss" scoped>
-  .notes {
+.notes {
   font-size: 14px;
   background: #f5f5f5;
   padding-left: 16px;
   display: flex;
   align-items: center;
+
   .name {
     padding-right: 16px;
   }
+
   input {
     height: 64px;
     flex-grow: 1;
--- src/components/Money/Types.vue
+++ src/components/Money/Types.vue
@@ -1,24 +1,46 @@
 <template>
   <div>
     <ul class="types">
-      <li class="selected">支出</li>
-      <li>收入</li>
+      <li :class="type === '-' && 'selected'"
+        @click="selectType('-')">支出
+      </li>
+      <li :class="type === '+' && 'selected'"
+          @click="selectType('+')">收入
+      </li>
     </ul>
   </div>
 </template>
 
-<script lang="ts">
+<script>
 export default {
-name: "Types"
-}
+  name: 'Types',
+  props: ['xxx'],
+  data() {
+    return {
+      type: '-'//减号表示支出，'+'表示收入
+    }
+  },
+  mounted(){
+    console.log(this.xxx);
+  },
+  methods:{
+    selectType(type) {
+      if(type !== '-' && type !== '+') {
+        throw new Error('type is unknown')
+      }
+      this.type = type
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-  .types{
+.types {
   background: #c4c4c4;
   display: flex;
   text-align: center;
   font-size: 24px;
+
   > li {
     width: 50%;
     height: 64px;
@@ -26,6 +48,7 @@ name: "Types"
     justify-content: center;
     align-items: center;
     position: relative;
+
     &.selected::after {
       content: '';
       position: absolute;
