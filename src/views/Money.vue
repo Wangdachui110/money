df361c7ab75b90af126c518673a5880d8d2d0e14
wdc9358 <1971571804@qq.com>
2020-11-01 20:15:22 +0800

    添加createAt字段

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,59 +1,64 @@
 <template>
-    <Layout class-prefix="layout">
-      {{record}}
-      <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
-      <Types :value.sync="record.type"/>
-      <Notes @update:value="onUpdateNotes"/>
-      <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
-    </Layout>
+  <Layout class-prefix="layout">
+    {{ recordList }}
+    <NumberPad :value.sync="record.amount" @submit="saveRecord"/>
+    <Types :value.sync="record.type"/>
+    <Notes @update:value="onUpdateNotes"/>
+    <Tags :data-source.sync="tags" @update:value="onUpdateTags"/>
+  </Layout>
 </template>
-
 <script lang="ts">
-  import Vue from 'vue';
-  import NumberPad from '@/components/Money/NumberPad.vue';
-  import Types from '@/components/Money/Types.vue';
-  import Notes from '@/components/Money/Notes.vue';
-  import Tags from '@/components/Money/Tags.vue';
-  import {Component, Watch} from 'vue-property-decorator';
+import Vue from 'vue';
+import NumberPad from '@/components/Money/NumberPad.vue';
+import Types from '@/components/Money/Types.vue';
+import Notes from '@/components/Money/Notes.vue';
+import Tags from '@/components/Money/Tags.vue';
+import {Component, Watch} from 'vue-property-decorator';
 
-  type Record = {
-    tags: string[];
-    notes: string;
-    type: string;
-    amount: number;
+const recordList: Record[] = JSON.parse(window.localStorage.getItem('recordList') || '[]');
+
+type Record = {
+  tags: string[];
+  notes: string;
+  type: string;
+  amount: number;
+  createAt?: Date;
+}
+
+@Component({
+  components: {Tags, Notes, Types, NumberPad}
+})
+export default class Money extends Vue {
+  tags = ['衣', '食', '住', '行'];
+  recordList: Record[] = recordList;
+  record: Record = {
+    tags: [], notes: '', type: '-', amount: 0
+  };
+
+  onUpdateTags(value: string[]) {
+    this.record.tags = value;
   }
 
-  @Component({
-    components: {Tags, Notes, Types, NumberPad}
-  })
-  export default class Money extends Vue{
-    tags = ['衣', '食', '住', '行'];
-    recordList: Record[] = [];
-    record: Record = {
-      tags: [], notes: '',type: '-', amount: 0
-    };
-
-    onUpdateTags(value: string[]){
-      this.record.tags = value;
-    }
-    onUpdateNotes(value: string) {
-      this.record.notes = value;
-    }
-    saveRecord(){
-      const record2 = JSON.parse(JSON.stringify((this.record)));
-      this.recordList.push(record2);
-      console.log(this.recordList);
-    }
-    @Watch('recordList')
-    onRecordListChange(){
-      window.localStorage.setItem('recordList',JSON.stringify(this.recordList));
-    }
+  onUpdateNotes(value: string) {
+    this.record.notes = value;
   }
+
+  saveRecord() {
+    const record2: Record = JSON.parse(JSON.stringify((this.record)));
+    record2.createAt = new Date();
+    this.recordList.push(record2);
+  }
+
+  @Watch('recordList')
+  onRecordListChange() {
+    window.localStorage.setItem('recordList', JSON.stringify(this.recordList));
+  }
+}
 </script>
 
 <style lang="scss">
-  .layout-content{
-    display: flex;
-    flex-direction: column-reverse;
-  }
+.layout-content {
+  display: flex;
+  flex-direction: column-reverse;
+}
 </style>
