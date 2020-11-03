b3d1f118b9c43593382dcb548deb14b60e6cac2b
wdc9358 <1971571804@qq.com>
2020-11-03 10:19:00 +0800

    添加EditLabel.vue的æ¼样式，抽离了两个组件Button和FormItem

--- src/components/Money/Tags.vue
+++ src/components/Money/Tags.vue
@@ -6,7 +6,7 @@
     <ul class="current">
       <li v-for="tag in dataSource" :key="tag"
           :class="{selected: selectedTags.indexOf(tag)>=0}"
-          @click="toggle(tag)">{{tag}}
+          @click="toggle(tag)">{{ tag }}
       </li>
     </ul>
   </div>
@@ -17,41 +17,45 @@ import Vue from 'vue';
 import {Component, Prop} from 'vue-property-decorator';
 
 @Component
-export default class Tags extends Vue{
+export default class Tags extends Vue {
   @Prop() readonly dataSource: string[] | undefined;
   selectedTags: string[] = [];
 
   toggle(tag: string) {
-    const index = this.selectedTags.indexOf(tag)
-    if(index>=0){
+    const index = this.selectedTags.indexOf(tag);
+    if (index >= 0) {
       this.selectedTags.splice(index, 1);
     } else {
       this.selectedTags.push(tag);
     }
-    this.$emit('update:value', this.selectedTags)
+    this.$emit('update:value', this.selectedTags);
   }
-  create(){
+
+  create() {
     const name = window.prompt('请输入标签名');
-    if(name === '') {
-      window.alert('标签名不能为空')
-    } else if(this.dataSource) {
-        this.$emit('update:dataSource',
-          [...this.dataSource,name]);
+    if (name === '') {
+      window.alert('标签名不能为空');
+    } else if (this.dataSource) {
+      this.$emit('update:dataSource',
+        [...this.dataSource, name]);
     }
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  .tags {
+.tags {
+  background: white;
   font-size: 14px;
   padding: 16px;
   flex-grow: 1;
   display: flex;
   flex-direction: column-reverse;
+
   > .current {
     display: flex;
     flex-wrap: wrap;
+
     > li {
       $bg: #d9d9d9;
       background: $bg;
@@ -62,7 +66,8 @@ export default class Tags extends Vue{
       padding: 0 16px;
       margin-right: 12px;
       margin-top: 4px;
-      &.selected{
+
+      &.selected {
         background: darken($bg, 50%);
         color: white;
       }
